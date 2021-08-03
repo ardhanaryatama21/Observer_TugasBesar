5c81cffa6434b76f9cfc085a70a6d1caf93012db
ardhan21 <ardhanaryatama21@gmail.com>
2021-08-04 04:28:07 +0700

    Tugas Besar CCDP Observer

--- src/notifikasi_observer_tugasbesar/Admin.java
+++ src/notifikasi_observer_tugasbesar/Admin.java
@@ -8,6 +8,10 @@ package notifikasi_observer_tugasbesar;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ *
+ * @author FR13NDS
+ */
 public class Admin implements Subject {
     private List<Pelanggan> pelanggan = new ArrayList<>();
     String pesan;
